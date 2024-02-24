a1aa44d77b1bc46244375055469f63d6ce314ec9
Hima2010 <88380671+Hima2010@users.noreply.github.com>
2024-02-24 16:08:05 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,5 @@
 def main():
-    # Ask the user for their name
-    name = input("What's your name? ")
-
-    # Greet the user
-    print("Hello, " + name + "! Nice to meet you.")
+    print("Hello Students", "! Nice to meet you.")
 
 if __name__ == "__main__":
     main()
